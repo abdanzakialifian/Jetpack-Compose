bb3811f34c2b335b91df8747572c1dc64784956d
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-19 18:12:42 +0700

    First commit in branch basic-jetpack-compose

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jetpack Compose")
+                    Greeting("Basic Jetpack Compose")
                 }
             }
         }
