9ef385b347cb0a2429ae9a0388f2c651f344005f
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-21 14:31:19 +0700

    Change structure code in MainActivity.kt

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -3,13 +3,10 @@ package com.compose.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,32 +17,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Jetpack Compose")
-                }
+                JetCoffeeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(contentAlignment = Alignment.Center) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
+fun JetCoffeeApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {}
 }
 
-@Preview(showBackground = true, name = "Greeting Preview", device = Devices.PIXEL_4)
+@Preview(showBackground = true, name = "Jet Coffee Preview", device = Devices.PIXEL_4)
 @Composable
-fun GreetingPreview() {
+fun JetCoffeeAppPreview() {
     JetpackComposeTheme {
-        Greeting("Jetpack Compose")
+        JetCoffeeApp()
     }
 }
