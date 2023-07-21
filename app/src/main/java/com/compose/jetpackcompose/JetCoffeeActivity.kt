c90699662dd0bb11fc88ad2e681b69c66f5488d2
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-21 15:32:33 +0700

    Add banner view

--- app/src/main/java/com/compose/jetpackcompose/JetCoffeeActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/JetCoffeeActivity.kt
@@ -3,16 +3,24 @@ package com.compose.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.compose.jetpackcompose.ui.components.Search
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
-class MainActivity : ComponentActivity() {
+class JetCoffeeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,7 +37,24 @@ fun JetCoffeeApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
-    ) {}
+    ) {
+        Column {
+            Banner()
+        }
+    }
+}
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            modifier = Modifier.height(160.dp),
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop
+        )
+        Search()
+    }
 }
 
 @Preview(showBackground = true, name = "Jet Coffee Preview", device = Devices.PIXEL_4)
