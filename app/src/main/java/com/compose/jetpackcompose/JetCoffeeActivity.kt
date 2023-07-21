92b2f2c14720861ae1e09ee8b0ab100658c69d5d
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-21 16:38:07 +0700

    Add menu view

--- app/src/main/java/com/compose/jetpackcompose/JetCoffeeActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/JetCoffeeActivity.kt
@@ -10,9 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -23,7 +24,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.compose.jetpackcompose.model.Menu
 import com.compose.jetpackcompose.ui.components.CategoryItem
+import com.compose.jetpackcompose.ui.components.MenuItem
 import com.compose.jetpackcompose.ui.components.Search
 import com.compose.jetpackcompose.ui.components.SectionText
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -46,10 +49,14 @@ fun JetCoffeeApp() {
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
-        Column {
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
             Banner()
             SectionText(title = stringResource(id = R.string.section_category))
             CategoryRow()
+            SectionText(title = stringResource(id = R.string.section_favorite_menu))
+            MenuRow(listMenu = DataDummy.dummyMenu)
+            SectionText(title = stringResource(id = R.string.section_best_seller_menu))
+            MenuRow(listMenu = DataDummy.dummyBestSellerMenu)
         }
     }
 }
@@ -76,6 +83,19 @@ fun CategoryRow(modifier: Modifier = Modifier) {
     ) {
         items(DataDummy.dummyCategory, key = { it.textCategory }) { category ->
             CategoryItem(category = category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
         }
     }
 }
