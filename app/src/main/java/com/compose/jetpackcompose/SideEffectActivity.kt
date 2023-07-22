646f65b0386c26e177395993a3cb65ac563bf10c
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-22 15:48:24 +0700

    Add navigation drawer menu

--- app/src/main/java/com/compose/jetpackcompose/SideEffectActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/SideEffectActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.launch
 
 class SideEffectActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +47,73 @@ class SideEffectActivity : ComponentActivity() {
 
 @Composable
 fun SideEffectApp() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
     // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
+        Scaffold(topBar = {
+            MyTopAppBar(onMenuClick = {
+                scope.launch {
+                    if (drawerState.isClosed) {
+                        drawerState.open()
+                    } else {
+                        drawerState.close()
+                    }
+                }
+            })
+        }) { paddingValues ->
+            ModalNavigationDrawer(
+                modifier = Modifier.padding(paddingValues),
+                drawerState = drawerState,
+                gesturesEnabled = drawerState.isOpen,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Text(text = stringResource(id = R.string.hello_from_nav_drawer))
+                    }
+                },
+                content = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        Column(modifier = Modifier.align(Alignment.Center)) {
+                            Text(text = stringResource(id = R.string.hello_world))
+                        }
+                    }
+                }
+            )
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(onMenuClick: () -> Unit, modifier: Modifier = Modifier) {
+    TopAppBar(
+        modifier = modifier,
+        navigationIcon = {
+            IconButton(onClick = onMenuClick) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(id = R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(text = stringResource(id = R.string.app_name))
+        })
 }
 
 @Preview(showBackground = true, name = "Greeting Preview", device = Devices.PIXEL_4)
 @Composable
 fun SideEffectAppPreview() {
     JetpackComposeTheme {
+        SideEffectApp()
     }
 }
