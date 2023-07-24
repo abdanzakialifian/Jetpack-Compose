c9a4042161ca55b9be07c32cd7bc511e54121010
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-24 21:32:15 +0700

    Finishing bottom navigation view

--- app/src/main/java/com/compose/jetpackcompose/JetRewardApp.kt
+++ app/src/main/java/com/compose/jetpackcompose/JetRewardApp.kt
@@ -1,15 +1,106 @@
 package com.compose.jetpackcompose
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.compose.jetpackcompose.ui.navigation.NavigationItem
+import com.compose.jetpackcompose.ui.navigation.Screen
+import com.compose.jetpackcompose.ui.screen.cart.CartScreen
+import com.compose.jetpackcompose.ui.screen.home.HomeScreen
+import com.compose.jetpackcompose.ui.screen.profile.ProfileScreen
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
 ) {
+    Scaffold(
+        modifier = modifier,
+        bottomBar = { BottomBar(navController) }) { paddingValues ->
+        NavHost(
+            modifier = Modifier.padding(paddingValues),
+            navController = navController,
+            startDestination = Screen.Home.route
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Cart.route) {
+                CartScreen()
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
+}
 
+@Composable
+fun BottomBar(navController: NavHostController, modifier: Modifier = Modifier) {
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar(modifier = modifier) {
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(id = R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(id = R.string.menu_cart),
+                icon = Icons.Default.ShoppingCart,
+                screen = Screen.Cart
+            ),
+            NavigationItem(
+                title = stringResource(id = R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            )
+        )
+
+        navigationItems.map { navigationItem ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = navigationItem.icon,
+                        contentDescription = navigationItem.title
+                    )
+                },
+                label = { Text(text = navigationItem.title) },
+                selected = currentRoute == navigationItem.screen.route,
+                onClick = {
+                    navController.navigate(navigationItem.screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        restoreState = true
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/compose/jetpackcompose/ui/navigation/Screen.kt
@@ -0,0 +1,7 @@
+package com.compose.jetpackcompose.ui.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Cart : Screen("cart")
+    object Profile : Screen("profile")
+}
