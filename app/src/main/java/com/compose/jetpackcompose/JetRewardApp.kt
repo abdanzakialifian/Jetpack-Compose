ad7fe2821c3056a8781cf33c503717fa422f7cd5
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-07-25 21:24:22 +0700

    Add list reward in home page and handle navigate to detail page

--- app/src/main/java/com/compose/jetpackcompose/JetRewardApp.kt
+++ app/src/main/java/com/compose/jetpackcompose/JetRewardApp.kt
@@ -17,32 +17,41 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.compose.jetpackcompose.ui.navigation.NavigationItem
 import com.compose.jetpackcompose.ui.navigation.Screen
 import com.compose.jetpackcompose.ui.screen.cart.CartScreen
+import com.compose.jetpackcompose.ui.screen.detail.DetailScreen
 import com.compose.jetpackcompose.ui.screen.home.HomeScreen
 import com.compose.jetpackcompose.ui.screen.profile.ProfileScreen
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun JetRewardApp(
-    modifier: Modifier = Modifier,
-    navController: NavHostController = rememberNavController()
+    modifier: Modifier = Modifier, navController: NavHostController = rememberNavController()
 ) {
-    Scaffold(
-        modifier = modifier,
-        bottomBar = { BottomBar(navController) }) { paddingValues ->
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(modifier = modifier, bottomBar = {
+        if (currentRoute != Screen.Detail.route) {
+            BottomBar(navController, currentRoute)
+        }
+    }) { paddingValues ->
         NavHost(
             modifier = Modifier.padding(paddingValues),
             navController = navController,
             startDestination = Screen.Home.route
         ) {
             composable(Screen.Home.route) {
-                HomeScreen()
+                HomeScreen(onNavigateToDetail = { rewardId ->
+                    navController.navigate(Screen.Detail.createRoute(rewardId))
+                })
             }
             composable(Screen.Cart.route) {
                 CartScreen()
@@ -50,29 +59,35 @@ fun JetRewardApp(
             composable(Screen.Profile.route) {
                 ProfileScreen()
             }
+            composable(Screen.Detail.route, arguments = listOf(navArgument("rewardId") {
+                type = NavType.LongType
+            })) {
+                val id = it.arguments?.getLong("rewardId") ?: -1L
+                DetailScreen(rewardId = id,
+                    navigateBack = { navController.navigateUp() },
+                    navigateToCart = {})
+            }
         }
     }
 }
 
 @Composable
-fun BottomBar(navController: NavHostController, modifier: Modifier = Modifier) {
-
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
-
+fun BottomBar(
+    navController: NavHostController,
+    currentRoute: String?,
+    modifier: Modifier = Modifier
+) {
     NavigationBar(modifier = modifier) {
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(id = R.string.menu_home),
                 icon = Icons.Default.Home,
                 screen = Screen.Home
-            ),
-            NavigationItem(
+            ), NavigationItem(
                 title = stringResource(id = R.string.menu_cart),
                 icon = Icons.Default.ShoppingCart,
                 screen = Screen.Cart
-            ),
-            NavigationItem(
+            ), NavigationItem(
                 title = stringResource(id = R.string.menu_profile),
                 icon = Icons.Default.AccountCircle,
                 screen = Screen.Profile
@@ -80,13 +95,11 @@ fun BottomBar(navController: NavHostController, modifier: Modifier = Modifier) {
         )
 
         navigationItems.map { navigationItem ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = navigationItem.icon,
-                        contentDescription = navigationItem.title
-                    )
-                },
+            NavigationBarItem(icon = {
+                Icon(
+                    imageVector = navigationItem.icon, contentDescription = navigationItem.title
+                )
+            },
                 label = { Text(text = navigationItem.title) },
                 selected = currentRoute == navigationItem.screen.route,
                 onClick = {
@@ -97,8 +110,7 @@ fun BottomBar(navController: NavHostController, modifier: Modifier = Modifier) {
                         restoreState = true
                         launchSingleTop = true
                     }
-                }
-            )
+                })
         }
     }
 }
--- app/src/main/java/com/compose/jetpackcompose/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/compose/jetpackcompose/ui/screen/home/HomeScreen.kt
@@ -1,22 +1,82 @@
 package com.compose.jetpackcompose.ui.screen.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.compose.jetpackcompose.R
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.compose.jetpackcompose.di.Injection
+import com.compose.jetpackcompose.model.FakeRewardDataSource
+import com.compose.jetpackcompose.model.OrderReward
+import com.compose.jetpackcompose.ui.ViewModelFactory
+import com.compose.jetpackcompose.ui.common.UiState
+import com.compose.jetpackcompose.ui.components.RewardItem
+import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(
+        factory = ViewModelFactory(
+            Injection.provideRepository()
+        )
+    ),
+    onNavigateToDetail: (Long) -> Unit
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+        when (uiState) {
+            is UiState.Error -> {}
+            is UiState.Loading -> viewModel.getAllRewards()
+            is UiState.Success -> HomeContent(
+                uiState.data,
+                modifier = modifier,
+                onNavigateToDetail = onNavigateToDetail
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeContent(
+    orderReward: List<OrderReward>,
+    modifier: Modifier = Modifier,
+    onNavigateToDetail: (Long) -> Unit
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Adaptive(160.dp),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(stringResource(R.string.menu_home))
+        items(orderReward) { data ->
+            RewardItem(
+                modifier = Modifier.clickable {
+                    onNavigateToDetail(data.reward.id)
+                },
+                image = data.reward.image,
+                title = data.reward.title,
+                requiredPoint = data.reward.requiredPoint
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeContentPreview() {
+    val orderRewards = mutableListOf<OrderReward>()
+    FakeRewardDataSource.dummyRewards.forEach {
+        orderRewards.add(OrderReward(it, 0))
+    }
+    JetpackComposeTheme {
+        HomeContent(orderReward = orderRewards, onNavigateToDetail = {})
     }
 }
