978992a10b6f01b47a3c9339afe3a519461409d5
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-08-01 15:40:12 +0700

    Compose UI Testing

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,32 +34,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Jetpack Compose")
-                }
+                CalculatorApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(contentAlignment = Alignment.Center) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+fun CalculatorApp(modifier: Modifier = Modifier) {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        var lengthInput by remember {
+            mutableStateOf("")
+        }
+        var widthInput by remember {
+            mutableStateOf("")
+        }
+        var result by remember {
+            mutableStateOf("")
+        }
+
+        val length = lengthInput.toDoubleOrNull() ?: 0.0
+        val width = widthInput.toDoubleOrNull() ?: 0.0
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp), contentAlignment = Alignment.Center
+        ) {
+            Column {
+                TextField(
+                    value = lengthInput,
+                    onValueChange = { lengthInput = it },
+                    label = {
+                        Text(
+                            text = stringResource(id = R.string.enter_length)
+                        )
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = widthInput,
+                    onValueChange = { widthInput = it },
+                    label = {
+                        Text(
+                            text = stringResource(id = R.string.enter_width)
+                        )
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = { result = (length * width).toString() }) {
+                    Text(text = stringResource(id = R.string.count))
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = stringResource(id = R.string.result, result)
+                )
+            }
+        }
     }
 }
 
 @Preview(showBackground = true, name = "Greeting Preview", device = Devices.PIXEL_4)
 @Composable
-fun GreetingPreview() {
+fun CalculatorAppPreview() {
     JetpackComposeTheme {
-        Greeting("Jetpack Compose")
+        CalculatorApp()
     }
 }
